87b6b04c70e97c55428ecfd141d461b4aa601c39
daniel kinny <danielkinny0214@gmail.com>
2025-03-28 16:30:38 +0400

    refactor product display: move product list to ProductBar component

---
+++ components/ui/productbar.tsx
@@ -0,0 +1,83 @@
+import React from "react";
+import {
+    Carousel,
+    CarouselItem,
+    CarouselContent,
+    CarouselNext,
+    CarouselPrevious,
+  
+  } from "./carousel";
+interface productType {
+    id: string;
+    name: string;
+    price: number;
+    front: string;
+    back: string;
+    }
+
+  const clothes : productType[] = [
+    {
+      id: "0001",
+      name: 'random school jacket [L]',
+      price: 25,
+      front: "/images/0001-1.png",
+      back: "/images/0001-2.png"
+    },
+    {
+      id: "0002",
+      name: 'england shirt [L]',
+      price: 30,
+      front: "/images/0002-1.png",
+      back: "/images/0002-2.png"
+    },
+    {
+      id: "0003",
+      name: 'random employee polo [M]',
+      price: 10,
+      front: "/images/0003-1.png",
+      back: "/images/0003-2.png"
+    }
+    ]
+const ProductBar = () => {
+    return (
+        <div className='border border-gray-500 rounded-lg p-4'>
+                  {
+                  clothes.map((product, index) => (
+                    <div key={index} className='p-4 border-b border-gray-300 max-w-2xl text-center'>
+                      <h2 className='text-2xl font-semibold'>{product.name}</h2>
+                      <div className='flex justify-center items-center'>
+                      
+                        <Carousel
+                opts={{
+                  align: "start",
+                }}
+                className="w-full max-w-sm p-4"
+              >
+                          <CarouselContent>
+                            <CarouselItem>
+                              <img
+                                src={product.front}
+                                className=" w-full rounded-lg"
+                              />
+                            </CarouselItem>
+                            <CarouselItem>
+                              <img
+                                src={product.back}
+                                className="w-full rounded-lg"
+                              />
+                            </CarouselItem>
+                          </CarouselContent>
+                          <CarouselPrevious className='bg-white border border-black hover:bg-black hover:text-white'/>
+                          <CarouselNext className='bg-white border border-black hover:bg-black hover:text-white'/>
+                        </Carousel>
+                      </div>
+                      
+                      <p className='text-xl font-bold'>{product.price +" dirhams"}</p>
+                    </div>
+                  ))
+        }
+                </div>
+    );
+}
+
+export default ProductBar;
